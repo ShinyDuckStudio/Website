cf31ea0e3f70db86b0f2be3bf9dd474cf9065975
Jules-Arthus "JA" Klein <jules.arthus.klein@gmail.com>
2024-06-11 17:31:43 +0200

    .

--- ndot/download/script.js
+++ ndot/download/script.js
@@ -1,4 +1,4 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
     // Replace 'your_game_file_path' with the actual path to your game file
-    window.location.href = '../bee-movie.txt';
+    window.location.href = './bee-movie.ndot';
 });
