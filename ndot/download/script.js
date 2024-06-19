f50777518138f7413e09450ccc9a25f4e4f830d5
ShinyDuck Studio <jularthus@shinyduckstudio.fr>
2024-06-19 23:20:50 +0200

    added manuals + fix typo + removed useless script + updated installer

--- ndot/download/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-    window.location.href = './bee-movie.ndot';
-});
-document.getElementById('manual').addEventListener('click', function() {
-    window.location.href = './bee-movie.ndot';
-});
