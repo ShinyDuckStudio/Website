f13a81f31d50eb915ed62387110bc5c4b92a0a2d
Esti <amandine.dos-santos@epita.fr>
2024-06-19 14:23:14 +0200

    fin ? (encore ??)

--- ndot/download/script.js
+++ ndot/download/script.js
@@ -1,4 +1,6 @@
 document.getElementById('downloadButton').addEventListener('click', function() {
-    // Replace 'your_game_file_path' with the actual path to your game file
+    window.location.href = './bee-movie.ndot';
+});
+document.getElementById('manual').addEventListener('click', function() {
     window.location.href = './bee-movie.ndot';
 });
